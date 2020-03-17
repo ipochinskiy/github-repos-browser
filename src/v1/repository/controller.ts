5ed46134b4df42e4e218e380ca174fcd271b6b3b
Ilia Pochinskii <ilia.pochinskii@metronom.com>
2020-03-18 00:42:36 +0100

    add repository service and call it from the controller

--- src/v1/repository/controller.ts
+++ src/v1/repository/controller.ts
@@ -1,14 +1,19 @@
 import { Request, Response } from 'express';
 
+import { HttpStatusCode } from '../../shared/http-status-codes';
+
+import { getRepositoryListByUsername } from './service';
+
 export const createRepositoryController = () => {
-  const getRepositoryList = (request: Request, response: Response) => {
-    const { username } = request.query;
-
-    // TODO: log it out
-    return response.send(`Hello ${username}!`);
-  };
-
   return {
-    getRepositoryList,
+    async getRepositoryList(request: Request, response: Response) {
+      // TODO: log it out
+
+      // TODO: validate username
+      const { username } = request.query;
+
+      const result: any = await getRepositoryListByUsername(username);
+      return response.status(HttpStatusCode.OK).json(result);
+    },
   };
 };
---
+++ src/v1/repository/service.ts
@@ -0,0 +1,48 @@
+import { RepositoryDTO } from './domain/repository-dto';
+
+import {
+  BranchRaw,
+  RepositoryRaw,
+  fetchBranchesByRepositoryFullName,
+  fetchRepositoriesByUsername,
+} from '../../shared/github-facade';
+
+const mapRawRepositoryAndBranchToDTO = (
+  rawRepository: RepositoryRaw,
+  rawBranchList: BranchRaw[] = [],
+): RepositoryDTO => {
+  try {
+    return {
+      name: rawRepository.name,
+      ownerLogin: rawRepository.owner.login,
+      branchList: rawBranchList.map(rawBranch => ({
+        name: rawBranch.name,
+        lastCommitSha: rawBranch.commit.sha,
+      })),
+    };
+  } catch (err) {
+    console.warn(`Repository "${rawRepository.full_name}" couldn't be converted to DTO`, err);
+    return null;
+  }
+};
+
+const promiseRepoBranchesAndMapToDTO = async (rawRepository: RepositoryRaw): Promise<RepositoryDTO> => {
+  const rawBranchList = await fetchBranchesByRepositoryFullName(rawRepository['full_name']);
+  return mapRawRepositoryAndBranchToDTO(rawRepository, rawBranchList);
+};
+
+export const getRepositoryListByUsername = async (username: string): Promise<RepositoryDTO[]> => {
+  let rawRepositoryList: RepositoryRaw[];
+  try {
+    rawRepositoryList = await fetchRepositoriesByUsername(username);
+  } catch (err) {
+    console.log('ERROR:', err);
+    rawRepositoryList = [];
+  }
+
+  const promisedRepositoryDTOList: Promise<RepositoryDTO>[] = rawRepositoryList
+    .filter(repo => !!repo && !repo.fork)
+    .map(promiseRepoBranchesAndMapToDTO);
+
+  return Promise.all(promisedRepositoryDTOList);
+};
